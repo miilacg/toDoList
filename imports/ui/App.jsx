f64aa722dec4fbd1fb9569cc84c350092a3021d7
Camila Guimarães <38961741+miilacg@users.noreply.github.com>
2021-06-11 17:19:00 -0300

    Formulários e eventos

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -2,16 +2,22 @@ import React from 'react';
 import { useTracker } from 'meteor/react-meteor-data'; //cada vez que os dados mudam por meio de reatividade, o componente será renderizado novamente
 
 import { TasksCollection } from '/imports/api/TasksCollection';
+import { TaskForm } from './TraskForm';
 import { Task } from './Task';
 
 
 
 export const App = () => {
-  const tasks = useTracker(() => TasksCollection.find({}).fetch());
+  const tasks = useTracker(() => 
+    TasksCollection.find({}, { sort: { createdAt: -1 } }).fetch())
+  ;
 
   return(
     <div>
       <h1>Welcome to Meteor!</h1>
+      
+      <TaskForm />
+
       <ul>
         { tasks.map(task => 
           <Task key={ task._id } task={ task }/>
