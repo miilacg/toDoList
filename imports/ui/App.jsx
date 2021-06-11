51efe75a65652450490e22ea562006a7a6c4e1aa
Camila Guimarães <38961741+miilacg@users.noreply.github.com>
2021-06-11 18:39:16 -0300

    Filtrar tarefas
    Tarefas filtradas por status

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useTracker } from 'meteor/react-meteor-data'; //cada vez que os dados mudam por meio de reatividade, o componente será renderizado novamente
 
 import { TasksCollection } from '/imports/api/TasksCollection';
@@ -19,22 +19,40 @@ const deleteTask = ({ _id }) => TasksCollection.remove(_id);
 
 
 export const App = () => {
+  const [hideCompleted, setHideCompleted] = useState(false);
+  const hideCompletedFilter = { isChecked: { $ne: true }};
+
   const tasks = useTracker(() => 
-    TasksCollection.find({}, { sort: { createdAt: -1 } }).fetch()
+    TasksCollection.find(hideCompleted ? hideCompletedFilter : {}, { 
+      sort: { createdAt: -1 },
+    }).fetch()
   );
+
+  const pendingTasksCount = useTracker(() => 
+    TasksCollection.find(hideCompletedFilter).count()
+  );
+
+  const pendingTasksTitle = `${ pendingTasksCount ? `(${ pendingTasksCount })` : '' }`;
+
 
   return(
     <div className='app'>
       <header>
         <div className='app-bar'>
           <div className='app-header'>
-            <h1>📝️ To do list</h1>
+            <h1> 📝️ To do list { pendingTasksTitle } </h1>
           </div>
         </div>
       </header>
       
       <div className='main'>
         <TaskForm />
+
+        <div className='filter'>
+          <button onClick={ () => setHideCompleted(!hideCompleted) }>
+            { hideCompleted ? 'Show All' : 'Hide Completed' }
+          </button>          
+        </div>
 
         <ul className='tasks'>
           { tasks.map(task => 
