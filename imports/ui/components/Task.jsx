988383863fd7404df8e9fb19602e35cf60869b48
Camila Guimarães <38961741+miilacg@users.noreply.github.com>
2021-06-16 12:26:39 -0300

    TASKS: melhoras na interface

--- imports/ui/components/Task.jsx
+++ imports/ui/components/Task.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 
 
-
 export const Task = ({ task, onCheckboxClick, onDeleteClick }) => {
 	return (
-		<li>
-			<input 
+		<li>			
+			<input
+				id={ `check${ task._id }`} 
 				type='checkbox'
 				checked={ !!task.isChecked }
 				onClick={ () => onCheckboxClick(task) }
 				readOnly
 			/>
-			
+
+			<label for={ `check${ task._id }`}>
+				<img src='/images/check.png' />
+			</label>		
+
 			<span> { task.text } </span>
 			<button onClick={ () => onDeleteClick(task) }> &times; </button> {/*Remover uma tarefa*/}
 		</li>
