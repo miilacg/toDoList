ab5d807dca7ef34476b5c78ed9e9cb38ee43d8c5
Camila Guimarães <38961741+miilacg@users.noreply.github.com>
2021-06-16 18:42:13 -0300

    USER: implementação do método de editar dados do usuário

--- imports/api/UserMethods.js
+++ imports/api/UserMethods.js
@@ -20,6 +20,36 @@ Meteor.methods ({
 		}
 	},
 	
+	'users.edit'(userId, username, password) {
+		check(userId, String);
+		check(username, String);
+		check(password, String);
+		
+		if (!this.userId) {
+			throw new Meteor.Error('Not authorized.');
+		}
+
+		const user = Meteor.users.findOne({ _id: userId });
+		if(user.username != username){
+			if(!Accounts.findUserByUsername(username)) {
+				Meteor.users.update({ _id: userId }, {
+					$set: {
+						username: username,
+						password: password, 
+					},
+				});
+			} else {
+				throw new Meteor.Error('Usuario já existe');
+			}	
+		} else { // se o username for igual ao user não precisa alterar
+			Meteor.users.update({ _id: userId }, {
+				$set: {
+					password: password, 
+				},
+			});
+		}			
+	},
+	
 	'users.remove'(userId) {
 		check(userId, String);
 
