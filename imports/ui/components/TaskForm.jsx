a0d58aa54dd43c7042d5a42c74fdd9418bda6413
Camila Guimarães <38961741+miilacg@users.noreply.github.com>
2021-06-22 18:44:12 -0300

    TASK FORM: refatoração do calculo da data

--- imports/ui/components/TaskForm.jsx
+++ imports/ui/components/TaskForm.jsx
@@ -4,12 +4,15 @@ import {
 	Button,
 	Checkbox, 
 	FormControl,
-	Input,
 	InputLabel,
 	ListItemSecondaryAction,
 	TextField	
 } from '@material-ui/core';
+import { Alert } from '@material-ui/lab';
 import { makeStyles } from '@material-ui/core/styles';
+
+import '../../../client/styles/forms.scss';
+
 
 
 
@@ -41,6 +44,7 @@ const useStyles = makeStyles((theme) => ({
 
 export function TaskForm({ action, taskId, buttonSubmit, buttonExit, onClickExit }) {
 	let currentDate = new Date();
+	const tempCurrentDate = currentDate.getTime();
 	const day = String(currentDate.getDate()).padStart(2, '0');
 	const month = String(currentDate.getMonth() + 1).padStart(2, '0');
 	const year = currentDate.getFullYear();
@@ -60,6 +64,14 @@ export function TaskForm({ action, taskId, buttonSubmit, buttonExit, onClickExit
 		e.preventDefault();
 
 		if(!titleTask || !date) return;
+
+		const tempDate = new Date(date);
+		if(tempDate.getTime() <= tempCurrentDate) {
+			const error = document.getElementById('error');
+			error.setAttribute("style", "display: flex");
+			return;
+		}
+
 		
 		if(action == 'create') { // Adicionando uma nova tarefa
 			Meteor.call('tasks.insert', date, titleTask, description, isParticular);
@@ -77,16 +89,14 @@ export function TaskForm({ action, taskId, buttonSubmit, buttonExit, onClickExit
 	
   
 	return (
-		<form className='task-form' onSubmit={ handleSubmit }>
-			<FormControl>
-				<InputLabel htmlFor="component-simple">Tarefa</InputLabel>
-				<Input 
-					id="component-simple" 
-					value={ titleTask } 
-					onChange={ (e) => setTitleTask(e.target.value) } 
-					required
-				/>
-			</FormControl>
+		<form className='form' onSubmit={ handleSubmit }>
+			<TextField						
+				value={ titleTask }
+				label="Tarefa"
+				type="text"
+				onChange={ (e) => setTitleTask(e.target.value) }
+				required
+			/>
 
 			<TextField
 				id="standard-textarea"
@@ -119,8 +129,14 @@ export function TaskForm({ action, taskId, buttonSubmit, buttonExit, onClickExit
 				</ListItemSecondaryAction>	
 			</FormControl>		
 
-			<Button variant="contained" onClick={ () => onClickExit() }>{ buttonExit }</Button>
-			<Button type='submit' variant="contained">{ buttonSubmit }</Button>
+			<Alert id='error' className='error' style={{ display:'none' }} severity="error">
+				Escolha uma data superior a data atual
+			</Alert>
+
+			<div>
+				<Button variant="contained" onClick={ () => onClickExit() }>{ buttonExit }</Button>
+				<Button type='submit' variant="contained">{ buttonSubmit }</Button>
+			</div>
 		</form>	 								
 	);
 };
