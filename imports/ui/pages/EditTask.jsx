92b9b785251de327c9e2df4735d8c7016f6780b9
Camila Guimarães <38961741+miilacg@users.noreply.github.com>
2021-06-23 16:35:20 -0300

    EDIT TASK: busca da tarefa

--- imports/ui/pages/EditTask.jsx
+++ imports/ui/pages/EditTask.jsx
@@ -1,21 +1,35 @@
 import React, { useState } from 'react';
+import { Meteor } from 'meteor/meteor';
+import { Tracker } from 'meteor/tracker'
 import { useParams } from "react-router-dom";
+import { useTracker } from 'meteor/react-meteor-data'; 
+
+import { TasksCollection } from '../../db/TasksCollection';
 
 import { Header } from '../components/Header';
 import { TaskForm } from '../components/TaskForm';
 
 
 
-export const EditTask = () => {
-	let { taskId } = useParams();
-
+export const EditTask = () => {	
 	const [state, setState] = useState(true); // true é para visualização
-
+	let { taskId } = useParams();
+	
 	const changeState = () => {
-		setState(!state); 
-  }
+		setState(!state);
+	}
+  
+	const task = useTracker(() => {
+		Meteor.subscribe('tasks');
+    const task = TasksCollection.findOne({ _id: taskId });
+		console.log('testUser ' + task);
 
-	return (
+    return task;
+  });
+
+	console.log('testUser ' + JSON.stringify(task));
+
+	return (			
 		<div className='app'>
 			<Header /> 
 			
