f0ed748e35d4b4489ac5fb6dfe9dc0f61751e3c7
Camila Guimarães <38961741+miilacg@users.noreply.github.com>
2021-06-24 18:35:11 -0300

    EDIT USER: criação da página de edição do usuario

--- imports/ui/pages/EditUserForm.jsx
+++ imports/ui/pages/EditUserForm.jsx
@@ -1,13 +1,40 @@
 import React, { useState } from 'react';
 import { Meteor } from 'meteor/meteor';
+import { Link } from 'react-router-dom';
+import { 
+	Button,
+	FormControl,
+	InputLabel,
+	TextField,
+	Select,
+	MenuItem	
+} from '@material-ui/core';
+import { Alert } from '@material-ui/lab';
 
 import { Header } from '../components/Header';
+
+import '../../../client/styles/forms.scss';
+import '../../../client/styles/user.scss';
 
 
 
 export const EditUserForm = () => {
+	let currentDate = new Date();
+	const tempCurrentDate = currentDate.getTime();
+	const day = String(currentDate.getDate()).padStart(2, '0');
+	const month = String(currentDate.getMonth() + 1).padStart(2, '0');
+	const year = currentDate.getFullYear();
+	const hour = currentDate.getHours();
+	const minute = String(currentDate.getMinutes()).padStart(2, '0');
+	currentDate = year + '-' + month + '-' + day + 'T' + hour + ':' + minute;
+
 	const [username, setUsername] = useState('');
 	const [password, setPassword] = useState('');
+	const [email, setEmail] = useState('');
+	const [date, setDate] = useState(currentDate);
+	const [gender, setGender] = useState('');
+	const [company, setCompany] = useState('');
+	const [photo, setPhoto] = useState('');
 
 	// Editando o usuario
 	const handleSubmit = e => {
@@ -15,9 +42,22 @@ export const EditUserForm = () => {
 
 		if(!username || !password) return;
 
-		Meteor.call('users.edit', username, password);
+		const tempDate = new Date(date);
+		if(tempDate.getTime() > tempCurrentDate) {
+			const error = document.getElementById('error');
+			error.setAttribute("style", "display: flex");
+			return;
+		}
+
+		Meteor.call('users.edit', username, password, email, date, gender, company);
+
 		setUsername('');
 		setPassword('');
+		setEmail('');
+		setDate(currentDate);
+		setGender('');
+		setCompany('');
+		setPhoto('');
 	};
 
 
@@ -25,26 +65,82 @@ export const EditUserForm = () => {
 		<div className='app'>
 			<Header /> 
 
-			<div className='main'>
-				<form onSubmit={ handleSubmit } className='login-form'>
-					<input 
-						type='text'
-						placeholder='Username'
-						value={ username }
-						required
-						onChange={ (e) => setUsername(e.target.value) }
-					/>
-				
-					<input 
-						type='password'
-						placeholder='Password'
+			<div className='main'>	
+				<h2>editar usuário</h2>
+
+				<form onSubmit={ handleSubmit } className='editUser'>
+					<div className='container'>
+						<TextField			
+							className='col-6'			
+							value={ username }
+							label="Nome"
+							type="text"
+							onChange={ (e) => setUsername(e.target.value) }
+							required
+						/>
+
+						<TextField
+							className='col-6'	
+							value={ email }
+							label="E-mail"
+							type="email"
+							onChange={ (e) => setEmail(e.target.value) }
+						/>
+					</div>
+
+					<div className='container'>
+					<TextField
+						className='col-4'	
 						value={ password }
-						required
+						label="Password"
+						type="password"
+						autoComplete="current-password"
 						onChange={ (e) => setPassword(e.target.value) }
+						required
+					/>				
+
+					<TextField
+						className='col-4'
+						value={ date }
+						label="Data de nascimento"
+						type="datetime-local"
+						onChange={ (e) => setDate(e.target.value) } 
+						InputLabelProps={{
+							shrink: true,
+						}}
+					/>	
+
+						<FormControl className='col-4'>
+							<InputLabel id="demo-simple-select-label">Sexo</InputLabel>
+							<Select
+								labelId="demo-simple-select-label"
+								value={ gender }
+								onChange={ (e) => setGender(e.target.value) }
+							>
+								<MenuItem value={ 'Feminino' }>Feminino</MenuItem>
+								<MenuItem value={ 'Masculino' }>Masculino</MenuItem>
+								<MenuItem value={ 'Prefiro não informar' }>Prefiro não informar</MenuItem>
+							</Select>
+						</FormControl>
+					</div>
+
+					<TextField
+						className='col-5'
+						value={ company }
+						label="Empresa"
+						type="text"
+						onChange={ (e) => setCompany(e.target.value) }
 					/>
 
-					<button type='submit'>Salvar alterações</button>	
-				</form> 
+					<Alert id='error' className='error' style={{ display:'none' }} severity="error">
+						Escolha uma data inferior a data atual
+					</Alert>
+
+					<div className='buttons'>	
+						<Button variant="contained"><Link to='/toDoList'>Voltar</Link></Button>
+						<Button type='submit' variant="contained">Salvar alterações</Button>
+					</div>
+				</form> 				
 			</div>
 		</div>
 	);
