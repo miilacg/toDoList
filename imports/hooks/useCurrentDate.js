ac3f57a24d1831249dcd32ed08a068847a31a046
Camila Guimarães <38961741+miilacg@users.noreply.github.com>
2021-06-25 12:24:59 -0300

    USE CURRENT DATE: mudança para tratar outras datas

--- imports/hooks/useCurrentDate.js
+++ imports/hooks/useCurrentDate.js
@@ -1,11 +1,17 @@
-export function useCurrentDate() {
-	let currentDate = new Date();
-	const tempCurrentDate = currentDate.getTime();
+export function useCurrentDate(currentDate) {
+
+	if(currentDate) {
+		currentDate = new Date(currentDate);
+	} else {
+		currentDate = new Date();
+	}
+
+	const tempDate = currentDate.getTime();
 	const day = String(currentDate.getDate()).padStart(2, '0');
 	const month = String(currentDate.getMonth() + 1).padStart(2, '0');
 	const year = currentDate.getFullYear();
 	const hour = currentDate.getHours();
 	const minute = String(currentDate.getMinutes()).padStart(2, '0');
 
-	return { tempCurrentDate, day, month, year, hour, minute }
+	return { tempDate, day, month, year, hour, minute }
 }
