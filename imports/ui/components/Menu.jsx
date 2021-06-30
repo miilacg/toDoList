1f72f55f4d6a9ce7481fbe549d25ed0f6e34040b
Camila Guimarães <38961741+miilacg@users.noreply.github.com>
2021-06-30 12:47:04 -0300

    MENU: subitens do drawer

--- imports/ui/components/Menu.jsx
+++ imports/ui/components/Menu.jsx
@@ -2,27 +2,41 @@ import React, { useState } from "react";
 import { Link, useHistory } from 'react-router-dom';
 
 import Button from '@material-ui/core/Button';
+import Collapse from '@material-ui/core/Collapse';
 import Drawer from '@material-ui/core/Drawer';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 
 import AccountCircleRoundedIcon from '@material-ui/icons/AccountCircleRounded';
-import HomeRoundedIcon from '@material-ui/icons/HomeRounded';
+import DashboardRoundedIcon from '@material-ui/icons/DashboardRounded';
+import DeleteOutlineRoundedIcon from '@material-ui/icons/DeleteOutlineRounded';
+import EditRoundedIcon from '@material-ui/icons/EditRounded';
+import ExitToAppRoundedIcon from '@material-ui/icons/ExitToAppRounded';
+import ExpandLess from '@material-ui/icons/ExpandLess';
+import ExpandMore from '@material-ui/icons/ExpandMore';
+import ListAltRoundedIcon from '@material-ui/icons/ListAltRounded';
+
+import '../../../client/styles/menu.scss';
 
 
 
 export function Menu({ createUser, pendingTasksTitle, user }) {
 	const history = useHistory();
+
+	const [openDrawer, setOpenDrawer] = useState(false);
 	const [open, setOpen] = useState(false);
 
-	const handleDrawer = (open) => (e) => {
+	const handleDrawer = (openDrawer) => (e) => {
     if (e.type === 'keydown' && (e.key === 'Tab' || e.key === 'Shift')) {
       return;
     }
 
-    setOpen(open);
+    setOpenDrawer(openDrawer);
+  };
+
+	const handleClick = () => {
+    setOpen(!open);
   };
 
 	const logout = () => {
@@ -40,7 +54,7 @@ export function Menu({ createUser, pendingTasksTitle, user }) {
 
   return (		
 		<>
-			{ !open ? (
+			{ !openDrawer ? (
 				<header className='app-bar'>
 					<div className='app-header'>
 						{ createUser ? ( // Se for a página de login	
@@ -59,6 +73,7 @@ export function Menu({ createUser, pendingTasksTitle, user }) {
 										{ user.username } 🚪 
 									</Button>
 								</>
+		
 							) : ( // Se for qualquer outra página
 								<Link to="/">
 									<h1>📝️ To do list { pendingTasksTitle }</h1>
@@ -68,29 +83,54 @@ export function Menu({ createUser, pendingTasksTitle, user }) {
 					</div>
 				</header>
 			) : (
-				<Drawer anchor='right' open={ open } onClose={ handleDrawer(false) }>
+				<Drawer anchor='right' open={ openDrawer } onClose={ handleDrawer(false) }>
 					<div
 						className='drawer'
-						role="presentation"
-						onClick={ handleDrawer(false) }
+						role="presentation"						
 						onKeyDown={ handleDrawer(false) }
 					>
 						<h1>📝️ To do list </h1>
 
 						<List>
-							<ListItem>
-								<ListItemIcon>
-									<HomeRoundedIcon /> 
-								</ListItemIcon>
-								<ListItemText primary='Home'/>
-							</ListItem>
+							<Link to='/dashboard'>
+								<ListItem>
+									<DashboardRoundedIcon /> 
+									<ListItemText primary='Dashboard'/>
+								</ListItem>
+							</Link>
 
-							<ListItem>
-								<ListItemIcon>
-									<AccountCircleRoundedIcon /> 
-								</ListItemIcon>
+							<Link to='/toDoList'>
+								<ListItem>
+									<ListAltRoundedIcon /> 
+									<ListItemText primary='Lista de tarefas'/>
+								</ListItem>
+							</Link>
+
+							<ListItem button onClick={ handleClick }>
+								<AccountCircleRoundedIcon /> 
 								<ListItemText primary='Perfil'/>
+								{ open ? <ExpandLess className='arrow' /> : <ExpandMore className='arrow'/> }
 							</ListItem>   
+							<Collapse in={ open } timeout="auto" unmountOnExit>
+								<List component="div" disablePadding>
+									<Link to={ `/editUser/${ user._id }` }>
+										<ListItem label='Editar usuário' className='nested'>
+											<EditRoundedIcon />
+											<ListItemText primary="Editar usuário" />
+										</ListItem>
+									</Link>
+
+									<ListItem label='Excluir usuário' onClick={ () => deleteUser(user._id) } className='nested'>
+										<DeleteOutlineRoundedIcon />
+										<ListItemText primary="Excluir usuário" />
+									</ListItem>
+
+									<ListItem label='Sair' onClick={ logout } className='nested'>
+										<ExitToAppRoundedIcon />
+										<ListItemText primary="Sair" />
+									</ListItem>
+								</List>
+							</Collapse>
 						</List>
 					</div>
 				</Drawer>
